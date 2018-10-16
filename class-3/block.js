827522915505c0d001e21462d0aa7b4e4dacca9e
SCYDA <>
2018-10-16 19:41:22 +0800

    class 2 finish, class 3 begin

---
+++ class-3/block.js
@@ -0,0 +1,36 @@
+var Block = function (position) {
+    // position 是 [0, 0] 格式
+    var p = position
+    var image = imageFromPath('block.png')
+    var o = {
+        image: image,
+        x: p[0],
+        y: p[1],
+        w: 40,
+        h: 20,
+        alive: true, // 砖块是否显示，默认是显示的
+        lifes: p[2] || 1  // 如果 p[2] 存在，不存在就是 1
+    }
+    // 砖块碰撞，不在显示
+    o.kill = function () {
+        o.lifes --
+        if (o.lifes < 1) {
+            o.alive = false
+        }
+    }
+    // 检测砖块和球是否已经碰撞了
+    o.collide = function (ball) {
+        var b = ball
+        // if (b.y > o.y && b.y < o.y + o.image.height) {
+        //     if (b.x > o.x && b.x < o.x + o.image.width) {
+        //         log('球和砖块相撞了')
+        //         return ture
+        //     }
+        // }
+        // return false
+
+        // 判断相撞，要有两次 o in b， b in o，并且砖块 alive = true
+        return o.alive && (rectIntersects(o, b) || rectIntersects(b, o))
+    }
+    return o
+}
---
+++ class-3/guagame.js
@@ -0,0 +1,64 @@
+// 一个 guagame 对象，用来加载画布
+var GuaGame = function (fps) {
+    var g = {
+        // 储存 key 和 function
+        actions: {},
+        // 储存按键的状态
+        keydowns: {},
+    }
+    // 拿到 canvas  
+    var canvas = document.querySelector('#id-canvas')
+    // 在 canvas 上指定 2d 图
+    var context = canvas.getContext('2d')
+
+    g.canvas = canvas
+    g.context = context
+
+    // draw 
+    g.drawImage = function (guaImage) {
+        g.context.drawImage(guaImage.image, guaImage.x, guaImage.y)
+    }
+
+    // event 按键状态,在 keydowns 储存
+    window.addEventListener('keydown', function (event) {
+        g.keydowns[event.key] = true
+    })
+    window.addEventListener('keyup', function (event) {
+        g.keydowns[event.key] = false
+    })
+    // 注册时间，按了那个 key 要执行什么函数, 在 setInterval 中调用
+    g.registerAction = function (key, callback) {
+        g.actions[key] = callback
+    }
+
+    // timer
+    window.fps = 30
+    var runloop = function() {
+        // log(window.fps)
+        // events  
+        var actions = Object.keys(g.actions)  // 获取到所有的 actions
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i]
+            if (g.keydowns[key]) {
+                // 如果按键按下，调用注册的 action 的 callback
+                g.actions[key]()
+            }
+        }
+        // update 更新
+        g.update()
+        // clear 清除画板
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        // drew 画图
+        g.draw()
+        // next run loop
+        // 递归调用 
+        setTimeout(function () {
+            runloop()
+        }, 1000 / window.fps)
+    }
+
+    setTimeout(function () {
+        runloop()
+    }, 1000 / fps)
+    return g
+}
