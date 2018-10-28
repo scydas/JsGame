546574fea46f79a96ea9c509050b55336dd7fd0f
SCYDA <>
2018-10-28 14:57:11 +0800

    烟花效果

---
+++ class-9/guagame/gua_scene.js
@@ -0,0 +1,50 @@
+class GuaScene {
+    constructor(game) {
+        this.game = game
+        this.debugModelEnable = true
+        this.elements = []
+    }
+    // 将子类中的共同方法提取到父类中
+    static new(game) {
+        var i = new this(game)
+        return i
+    }
+    // 父类中的 draw ()
+    draw() {
+        for (var i = 0; i < this.elements.length; i++) {
+            var e = this.elements[i]
+            this.game.drawImage(e)       
+        }
+    }
+    addElement(img) {
+        img.scene = this
+        this.elements.push(img)
+    }
+    update() {
+        if (this.debugModelEnable) {
+            for (var i = 0; i < this.elements.length; i++) {
+                var e = this.elements[i]
+                e.debug && e.debug()
+            }
+        }
+        for (var i = 0; i < this.elements.length; i++) {
+            var e = this.elements[i]
+            e.update()
+        }
+    }
+}
+
+// 继承父类 GuaScene
+// class SceneTitle extends GuaScene {
+//     constructor(game) {
+//         super(game)
+//         game.registerAction('k', function() {
+//             var s = Scene(game)
+//             game.replaceScene(s)
+//         })
+//     }
+//     draw() {
+//         // draw lables
+//         this.game.context.fillText('按 k 开始游戏', 150, 200)
+//     }
+// }
---
+++ class-9/scene/title/title.js
@@ -0,0 +1,37 @@
+// var SceneTitle = function (game) {
+//     var s = {
+//         game: game,
+//     }
+//     // 注册开始事件
+//     game.registerAction('k', function(){
+//         var s = Scene(game)
+//         game.replaceScene(s)
+//     })
+//     s.draw = function () {
+//         // draw labels 
+//         game.context.fillText('按 k 开始游戏', 150, 200)
+//     }
+//     s.update = function () {
+//     }
+//     return s
+// }
+
+// 继承父类 GuaScene
+class SceneTitle extends GuaScene {
+    constructor(game) {
+        super(game)
+        game.registerAction('k', function () {
+            var s = Scene(game)
+            game.replaceScene(s)
+        })
+    }
+    // 使用 SceneTitle.new() 替代 new SceneTitle() 使用方式
+    // static new(game) {
+    //     var i = new this(game)
+    //     return i
+    // }
+    draw() {
+        // draw lables
+        this.game.context.fillText('按 k 开始游戏', 150, 200)
+    }
+}
